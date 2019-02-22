7b6423503a240f87f6198c9eaf8c1cee7949da99
kuranadomoe <Wearestardust404@outlook.com>
2019-02-22 22:04:36 +0800

    dedicated server library

---
+++ DSMgrLibrary/CommonComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kuranado.moe.DedicatedServerManager.Library
+{
+    class CommonComponent
+    {
+
+    }
+}
---
+++ DSMgrLibrary/IDedicatedServerModule.cs
@@ -0,0 +1,11 @@
+﻿namespace kuranado.moe.DedicatedServerManager.Library
+{
+    /// <summary>
+    /// <para>饥荒独立服模块接口</para>
+    /// <para>此接口定义了每个模块所必须实现的内容</para>
+    /// </summary>
+    public interface IDedicatedServerModule
+    {
+
+    }
+}
