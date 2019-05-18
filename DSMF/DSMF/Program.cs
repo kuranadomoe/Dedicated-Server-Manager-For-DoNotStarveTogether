4e093c54adb615b147cdd54dafe28df06656c593
kuranadomoe <Wearestardust404@outlook.com>
2019-05-19 01:14:08 +0800

    change project structure

---
+++ DSMF/DSMF/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using kuranado.moe.DedicatedServerManager.Library;
+using static System.Console;
+
+namespace kuranado.moe.DedicatedServerManager.Server.MainModule
+{
+    class Program:IDedicatedServerModule
+    {
+        public static string ModuleName => "ServerMainModule";
+        private static TraceLogs Logs { get; } = new TraceLogs(ModuleName);
+
+
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "MainThread";
+
+            Logs.AddTextListener("Test", "nya.log", SourceLevels.Verbose);
+            Logs.Verbose("喵喵喵~~~");
+
+            Pause();
+        }
+
+
+        [Conditional("DEBUG")]
+        private static void Pause()
+        {
+            WriteLine("Press any key continue ... ");
+            ReadKey(true);
+        }
+    }
+}
