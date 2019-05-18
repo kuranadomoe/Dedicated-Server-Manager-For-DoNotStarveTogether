f70af6f3d93278690e09a0f4283e9987f089eb3f
kuranadomoe <Wearestardust404@outlook.com>
2019-05-19 00:52:01 +0800

    delete old code

--- BashCode/dst_shutdown_hikarimimamorusakamichide.sh
+++
@@ -1,3 +0,0 @@
-screen -X -S dst_hikarisakamichi_caves stuff 'c_shutdown()\n'
-screen -X -S dst_hikarisakamichi_overworld stuff 'c_shutdown()\n'
-
--- BashCode/dst_update.sh
+++
@@ -1,2 +0,0 @@
-cd /home/steam/steamcmd
-./steamcmd.sh +login anonymous +force_install_dir /home/steam/dstserver +app_update 343050 validate +quit
